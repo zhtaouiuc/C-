54e9110c28130bcea74d3da9ee3c6e1d994ab1de
Zhen Tao <coraline@Zhens-iMac.local>
2017-09-07 16:53:12 -0500

    Add Hello World code

---
+++ projects/HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Zhen Tao on 8/4/17.
+//  Copyright © 2017 Coraline. All rights reserved.
+//
+
+#include <iostream>
+//this program prints Hello World to the screen
+int main()
+{
+    using namespace std;
+    cout << "Hello World \n" ;
+    return 0;
+}
