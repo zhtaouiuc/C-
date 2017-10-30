693f2eb9f3c2c16c6ffd32039cebe13dd8043dc0
Hammes-Schiffer Group <SHSgroup@Hammes-Schiffers-MacBook-Pro.local>
2017-10-30 12:28:30 -0500

    updates on hartreefock program

--- projects/hf/hartreefock.cpp
+++ projects/hf/hartreefock.cpp
@@ -12,6 +12,8 @@
 #include <string>
 using namespace std;
 
+typede::Matrix<double, Eigen::Dynamicgen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> Matrix;, Eigen::Dynamic, Eigen::RowMajor> Matrix;
+
 //constructors
 hartreefock::hartreefock()
 {
@@ -136,16 +138,142 @@ cout << "  " << core[i][j] << "  ";
 }
 cout << endl;
 }
+}
 
-//for (int i=0; i<num; i++)
-//cout << core[i][i];
+void hartreefock::store2e(const char *m_2edat)
+{
+FILE *input;
+double val;
+int super = num*(num+1)/2+num;
+int *index = new int [super];
+int i =0;
+int j=0;
+int k=0;
+int l=0;
+int ij =0;
+int kl =0;
+int ijkl =0;
+TEI = new double [super*(super+1)/2+super];
+
+index[0] = 0;
+for (int i=1; i< super; i++)
+index[i] = index[i-1] +i ;
+ 
+input = fopen(m_2edat,"r");
+while (fscanf(input, "%d %d %d %d %lf", &i, &j, &k, &l, &val) != EOF)
+{
+ij = (i>j) ? index[i]+j : index[j]+i;
+kl = (k>l) ? index[k]+l : index[l]+l;
+ijkl = (ij>kl) ? index[ij]+kl : index[kl]+ij;
+//cout << " i " <<i<< " j " <<j<<" ij "<< ij <<" k " << k << " l " <<l<< " kl "<<kl<<" ijkl " <<ijkl << endl;
+TEI[ijkl] = val;
+//cout<< " ijkl " <<ijkl << " val " << val<<endl;
+}
 }
 
 
+void hartreefock::matrixmu(double **ma, double **mb, double **mc,int rownuma, int colnuma, int rownumb, int colnumb)
+{
+if (colnuma != rownumb) {cout << "error in the multiplication."<<endl;}
+for (int i=0; i<rownuma; i++)
+{
+for (int j=0; j<colnumb; j++)
+{mc[i][j] = 0.0;
+{for (int k=0; k<colnuma; k++)
+mc[i][j] += ma[i][k] * mb[k][j];
+}
+}
+}
+}
+void hartreefock::orthomatrix()
+{
+
+double **eigenvalues = new double *[num];
+double **eigenvectors = new double *[num];
+
+for (int i = 0; i< num ; i++)
+{eigenvalues[i]= new double [num];
+eigenvectors[i] = new double [num];
+}
+
+Matrix I (num, num);
+for (int i =0; i<num; i++)
+{
+for (int j=0; j<num; j++)
+I(i,j) = overlap[i][j];
+}
+Eigen::SelfAdjointEigenSolver<Matrix> solver(I);
+  Matrix evecs = solver.eigenvectors();
+  Matrix evals = solver.eigenvalues();
+for (int i = 0 ; i<num; i++)
+{
+eigenvalues[i][i] = pow(evals(i),-0.5);
+for (int j=0; j<num; j++)
+eigenvectors[i][j]= evecs(i,j);
+}
+
+ortho = new double *[num];
+for (int i = 0; i< num ; i++)
+ortho[i] = new double [num];
+
+matrixmu(eigenvalues,eigenvectors,ortho, num, num, num, num);
+matrixmu(eigenvectors, ortho, ortho, num,num,num, num);
+
+}
+
+void hartreefock::deninitial();
+{
+if (iter == 0)
+{
+double **fockinitial = new double *[num];
+for (int i=0; i< num; i++)
+fockinitial[i] = new double [num];
+
+matrixmu(core,ortho,fockinitial,num,num,num,num);
+matrixmu(ortho,fockinitial,num,num,num,num);
+
+Matrix F0 (num,num);
+
+for (int i =0; i<num; i++)
+{
+for (int j=0; j<num; j++)
+F0(i,j) = fockinitial[i][j];
+}
+
+Eigen::SelfAdjointEigenSolver<Matrix> solver(I);
+  Matrix evecs = solver.eigenvectors();
+  Matrix evals = solver.eigenvalues();
+
+double **eigenvectorsini = new double *[num];
+double **temp = new double *[temp];
+for (int i = 0; i< num ; i++)
+{
+eigenvectorsini[i] = new double [num];
+temp[i] = new double [num];
+for (int j=0; j<num; j++)
+{
+eigenvectorsini[i][j] = evecs(i,j);
+}}
+
+matrixmu(ortho,eigenvectorsini,temp,num,num,num,num);
 
 
+double **densa = new double *[num];
+for (int i=0; i< num; i++)
+densa[i] = new double [num];
 
+for (int i=0; i< num; i++)
+{
+for (int j=0; j< num; j++)
+{
+for (int k=0; k< nocc; k++)
+densa[i][j]+= temp[i][k]*temp[k][j];
+}
+}
+}
 
+else 
+{
 
-
-
+}
+}
--- projects/hf/hartreefock.h
+++ projects/hf/hartreefock.h
@@ -4,21 +4,44 @@
 class hartreefock
 {
 private:
-    char enucdat[30];
-    double enuc;
+// build Hcore
     char overlapdat[100];
     double **overlap;
     char kineticdat[30];
     double **kinetic;
     char nucleardat[30];
     double **nuclear;
-    double **core;
+    double **core;    
     int num; //number of AO basis set
+
+// read in nuclear energy
+    char enucdat[30];
+    double enuc;
+
+// read in two electron integral
+    double *TEI;
+
+// build orthogonal matrix 
+    double **ortho;
+    void orthomatrix();
+
+// build density matrix
+    int nocc; //number of occupied orbitals
+    int iter; //number of iteration 
+    double **fock; 
+    double **densa;
+    double **densb;
+    void deninitial();
+    
+// matrix multiplication
+    void matrixmu(double **ma, double **mb, double **mc,int rownuma, int colnuma, int rownumb, int colnumb);
+      
 public:
     hartreefock();
     hartreefock(const char *m_enucdat, const char *m_overlapdat, const char *m_kineticdat,
                 const char *m_nucleardat);
     ~hartreefock();
-    void print1e();
+    void print1e();  //build Hcore
+    void store2e(const char *m_2edat);  //store 2e integral
 };
 #endif
